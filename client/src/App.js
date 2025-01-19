78e19aaa529e7e2de8bf9170a339e446329b80bf
Patrick Ly <patrickly@dhcp-128-189-202-183.ubcsecure.wireless.ubc.ca>
2025-01-18 22:54:18 -0800

    Added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,71 @@
+// import React, { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+
+// import LoginPage from './components/LoginPage';
+// import SharkDashboard from './components/SharkDashboard';
+// import Messages from './components/Messages';
+// import PitcherDashboard from './components/PitcherDashboard';
+// import Profile from './components/Profile';
+// import Navigation from './components/Navigation';
+// import SelectAcc from './components/SelectAcc';
+// import "./components/style/App.css";
+
+
+// function App() {
+//   // We’ll store the current user in App state for simplicity
+//   const [currentUser, setCurrentUser] = useState(null);
+
+//   return (
+//     <div id='app'>
+//       <Navigation currentUser={currentUser} setCurrentUser={setCurrentUser} />
+//       <Routes>
+//         {/* HomePage for signup/login */}
+//         <Route path="/" element={<SelectAcc setCurrentUser={setCurrentUser} />} />
+//         {/* LoginPage for signup/login */}
+//         <Route path="/login" element={<LoginPage setCurrentUser={setCurrentUser} />} />
+
+//         {/* Shark dashboard */}
+//         <Route
+//           path="/shark"
+//           element={<SharkDashboard currentUser={currentUser} />}
+//         />
+
+//         {/* Pitcher dashboard */}
+//         <Route
+//           path="/pitcher"
+//           element={<PitcherDashboard currentUser={currentUser} />}
+//         />
+
+//         {/* Messages */}
+//         <Route
+//           path="/messages"
+//           element={
+//             currentUser ? (
+//               <Messages currentUser={currentUser} />
+//             ) : (
+//               <div>Please log in to view messages.</div>
+//             )
+//           }
+//         />
+
+//         {/* Profile */}
+//         <Route
+//           path="/profile"
+//           element={
+//             currentUser ? (
+//               <Profile currentUser={currentUser} />
+//             ) : (
+//               <div>Please log in to view your profile.</div>
+//             )
+//           }
+//         />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
@@ -6,31 +74,34 @@ import SharkDashboard from './components/SharkDashboard';
 import Messages from './components/Messages';
 import PitcherDashboard from './components/PitcherDashboard';
 import Profile from './components/Profile';
+import Home from './components/Home'; // Import Home component
 import Navigation from './components/Navigation';
 import SelectAcc from './components/SelectAcc';
 import "./components/style/App.css";
 
-
 function App() {
-  // We’ll store the current user in App state for simplicity
   const [currentUser, setCurrentUser] = useState(null);
 
   return (
-    <div id='app'>
+    <div id="app">
       <Navigation currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <Routes>
-        {/* HomePage for signup/login */}
+        {/* Home Page */}
+        <Route path="/Home" element={<Home currentUser={currentUser} />} />
+
+        {/* Select Account */}
         <Route path="/" element={<SelectAcc setCurrentUser={setCurrentUser} />} />
-        {/* LoginPage for signup/login */}
+
+        {/* Login Page */}
         <Route path="/login" element={<LoginPage setCurrentUser={setCurrentUser} />} />
 
-        {/* Shark dashboard */}
+        {/* Shark Dashboard */}
         <Route
           path="/shark"
           element={<SharkDashboard currentUser={currentUser} />}
         />
 
-        {/* Pitcher dashboard */}
+        {/* Pitcher Dashboard */}
         <Route
           path="/pitcher"
           element={<PitcherDashboard currentUser={currentUser} />}
