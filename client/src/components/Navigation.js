2c02d1c73b66955fb14e1a74e911b603ffe162a9
Jennifer Tjen <jennifer.s.e.tjen@gmail.com>
2025-01-18 16:54:56 -0800

    navbar styling - waiting on routes

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as ExploreIcon } from "../assets/Icons/ExploreIcon.svg";
+import { ReactComponent as HomeIcon } from "../assets/Icons/HomeIcon.svg";
+import { ReactComponent as InboxIcon } from "../assets/Icons/InboxIcon.svg";
+import { ReactComponent as ProfileIcon } from "../assets/Icons/ProfileIcon.svg";
 
 const Navigation = ({ currentUser, setCurrentUser }) => {
   const handleLogout = () => {
@@ -7,20 +11,53 @@ const Navigation = ({ currentUser, setCurrentUser }) => {
   };
 
   return (
-    <nav style={{ borderBottom: '1px solid #ccc', marginBottom: '1rem' }}>
-      <Link to="/">Home</Link>
-      {currentUser && currentUser.type === 'shark' && (
-        <Link to="/shark" style={{ marginLeft: '1rem' }}>
+    <nav
+      style={{
+        height: "60px",
+        width: "300px",
+        position: "fixed",
+        zIndex: "1000",
+        backgroundColor: "Black",
+        borderRadius: "100px",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          paddingLeft: "40px",
+          paddingRight: "40px",
+        }}
+      >
+        <Link to="/">
+          <HomeIcon width="24" height="24" fill="white" />
+        </Link>
+        <Link to="/shark">
+          <ExploreIcon width="24" height="24" fill="white" />
+        </Link>
+        <Link to="/shark">
+          <InboxIcon width="24" height="24" fill="white" />
+        </Link>
+        <Link to="/shark">
+          <ProfileIcon width="24" height="24" fill="white" />
+        </Link>
+      </div>
+
+      {currentUser && currentUser.type === "shark" && (
+        <Link to="/shark" style={{ marginLeft: "1rem" }}>
           Shark Dashboard
         </Link>
       )}
-      {currentUser && currentUser.type === 'pitcher' && (
-        <Link to="/pitcher" style={{ marginLeft: '1rem' }}>
+      {currentUser && currentUser.type === "pitcher" && (
+        <Link to="/pitcher" style={{ marginLeft: "1rem" }}>
           Pitcher Dashboard
         </Link>
       )}
       {currentUser && (
-        <button onClick={handleLogout} style={{ marginLeft: '1rem' }}>
+        <button onClick={handleLogout} style={{ marginLeft: "1rem" }}>
           Logout
         </button>
       )}
